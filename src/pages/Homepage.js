2831bcebd97dd1c798c690fe4e17cac4f19dce08
Andy Aydin <andy@betcloud.com.au>
2022-11-26 10:53:44 +1100

    rest

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,11 @@
 const Homepage = () => (
-  <header
-    className="absolute z-10 w-auto min-w-full min-h-full max-w-none p-0 m-0"
-    style={{ width: "103%" }}
-  >
+  <div className="vimeo-wrapper">
     <iframe
       src="https://player.vimeo.com/video/773517659?h=7e877e28f8&amp;playsinline=0&amp;muted=1&amp;loop=1&amp;background=1&amp;quality=1080p&amp;app_id=122963"
       className="absolute z-10 w-auto min-w-full min-h-full max-w-full"
       height="100%"
     ></iframe>
-  </header>
+  </div>
 );
 
 export default Homepage;
